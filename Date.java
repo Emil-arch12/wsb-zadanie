9ce5a478d9bf96d93c81bf52df8650571cfaf122
Emil Buraczynski <emil.buraczynski@softman.pl>
2025-02-01 12:35:14 +0100

    nowe pliki

---
+++ Date.java
@@ -0,0 +1,8 @@
+import java.util.Stack;
+
+public class Date {
+    String date;
+    public Date(String date) {
+        this.date = date;
+    }
+}
---
+++ nowa.java
@@ -0,0 +1,4 @@
+public class nowa {
+    private String nome;
+    private int idade;
+}
